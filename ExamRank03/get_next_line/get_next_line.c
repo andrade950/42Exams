14eb9bf384c0d1f5897e759e626873fc6e58a9a2
João Miguel Andrade <157920929+andrade950@users.noreply.github.com>
2025-04-02 16:45:51 +0100

    Update get_next_line.c

--- ExamRank03/get_next_line/get_next_line.c
+++ ExamRank03/get_next_line/get_next_line.c
@@ -10,12 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
-#endif
-
-#include <stdlib.h>
-#include <unistd.h>
+#include "get_next_line.h"
 
 char	*ft_strdup(char *str)
 {
@@ -76,26 +71,12 @@ int	main(int argc, char **argv)
 {
 	int		fd;
 	char	*line;
-
-	if (argc != 2)
-	{
-		printf("Uso: %s <ficheiro>\n", argv[0]);
-		return (1);
-	}
-
 	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
-	{
-		perror("Erro ao abrir o ficheiro");
-		return (1);
-	}
-
 	while ((line = get_next_line(fd)))
 	{
 		printf("%s", line);
 		free(line);
 	}
-
 	close(fd);
 	return (0);
 } */
