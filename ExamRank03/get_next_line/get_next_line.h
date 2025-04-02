775a72408e1eaaede480f484f59dbd390ad4f2be
João Miguel Andrade <157920929+andrade950@users.noreply.github.com>
2025-04-02 16:45:13 +0100

    Create get_next_line.h

---
+++ ExamRank03/get_next_line/get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joaomart <joaomart@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/21 10:10:25 by joaomart          #+#    #+#             */
+/*   Updated: 2025/03/21 11:39:13 by joaomart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif
+
+#include <unistd.h>
+#include <stdlib.h>
+
+#endif
